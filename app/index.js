640a53cb03dccbe3d7c3585c1b26f3334838230e
Dmitry Volkov <dvolkov@tula.co>
2017-12-10 20:20:09 +0300

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter, Route, IndexRoute } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { AppContainer } from 'react-hot-loader';
+
+import store from './store';
+
+import App from './app';
+
+const render = (Component) => {
+    ReactDOM.render(
+        <AppContainer>
+            <Provider store={store}>
+                <HashRouter>
+                    <Route path="/" component={Component}>
+                    </Route>
+                </HashRouter>
+            </Provider>
+        </AppContainer>,
+        document.getElementById('root')
+    );
+};
+
+render(App);
+
+if (module.hot) {
+    module.hot.accept('./app', () => {
+        render(App)
+    });
+}
